5bc3877879b5dea9fe9697508c36f4caa9502596
inventivetalent <inventivetalent@inventivegames.de>
2016-05-10 03:15:40 +0200

    update BasicInputBox

--- MapMenus/src/main/resources/component/BasicInputBox.js
+++ MapMenus/src/main/resources/component/BasicInputBox.js
@@ -1,32 +1,37 @@
-var JavaColor = Java.type("java.awt.Color");
+// BasicInputBox
+// author: inventivetalent
+({
+    classes: {
+        JavaColor: Java.type("java.awt.Color")
+    },
+    click: function(player, relative) {
+        if (this.states.toggle("typing", player)) {
+            var thisRef = this;
+            this.menu.requestKeyboardInput(player, function(input) {
+                thisRef.states.remove("typing", player);
+                thisRef.data.put("typedText", player, input);
+            }, true);
+        }
+    },
+    render: function(graphics, player) {
+        var typedText = this.data.get("typedText", player);
+        var bounds = this.component.getBounds();
 
-function click(player, relative) {
-  states.put("typing", player);
-  menu.requestKeyboardInput(player, function(input) {
-    states.delete("typing", player);
-    data.put("typedText", player, input);
-  }, true);
-}
+        graphics.setColor(this.classes.JavaColor.DARK_GRAY);
+        graphics.drawLine(bounds.x + 5, bounds.y + bounds.height - 1, bounds.x + bounds.width - 5, bounds.y + bounds.height - 1);
 
-function render(graphics, player) {
-  var typedText = data.get("typedText", player);
+        if (this.states.get("typing", player)) {
+            graphics.setColor(this.classes.JavaColor.CYAN);
+            graphics.fillRect(bounds.x + 2, bounds.y + 2, bounds.width - 4, bounds.height - 4);
 
-  var bounds = component.getBounds();
-
-  graphics.setColor(JavaColor.DARK_GRAY);
-  graphics.drawLine(bounds.x + 5, bounds.y + bounds.height - 1, bounds.x + bounds.width - 5, bounds.y + bounds.height - 1);
-
-  if (states.get("typing", player)) {
-    graphics.setColor(JavaColor.CYAN);
-    graphics.fillRect(bounds.x + 2, bounds.y + 2, bounds.width - 4, bounds.height - 4);
-
-    graphics.setColor(JavaColor.gray);
-    graphics.drawString("Typing...", Math.round(bounds.x + 2), Math.round((bounds.y + bounds.height) - 5));
-  } else if (typedText !== null) {
-    graphics.setColor(JavaColor.black);
-    graphics.drawString(typedText, Math.round(bounds.x + 2), Math.round((bounds.y + bounds.height) - 5));
-  } else {
-    graphics.setColor(JavaColor.gray);
-    graphics.drawString("Click to type!", Math.round(bounds.x + 2), Math.round((bounds.y + bounds.height) - 5));
-  }
-}
+            graphics.setColor(this.classes.JavaColor.gray);
+            graphics.drawString("Typing...", Math.round(bounds.x + 2), Math.round((bounds.y + bounds.height) - 5));
+        } else if (typedText !== null) {
+            graphics.setColor(this.classes.JavaColor.black);
+            graphics.drawString(typedText, Math.round(bounds.x + 2), Math.round((bounds.y + bounds.height) - 5));
+        } else {
+            graphics.setColor(this.classes.JavaColor.gray);
+            graphics.drawString("Click to type!", Math.round(bounds.x + 2), Math.round((bounds.y + bounds.height) - 5));
+        }
+    }
+});
