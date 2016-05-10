393b470f74fab05d18bc65f390d921a2d5bdb15a
inventivetalent <inventivetalent@inventivegames.de>
2016-05-10 02:44:08 +0200

    update BasicCheckbox

--- MapMenus/src/main/resources/component/BasicCheckbox.js
+++ MapMenus/src/main/resources/component/BasicCheckbox.js
@@ -1,20 +1,22 @@
-var JavaColor = Java.type("java.awt.Color");
+({
+  classes: {
+    JavaColor: Java.type("java.awt.Color")
+  },
+  click: function(player, relative) {
+    this.states.toggle("checkboxChecked", player);
+  },
+  render: function(graphics, player) {
+    var checked = this.states.get("checkboxChecked", player);
 
-function click(player, relative) {
-  states.toggle("checkboxChecked", player);
-}
+    var bounds = this.component.getBounds();
 
-function render(graphics, player) {
-  var checked = states.get("checkboxChecked", player);
+    graphics.setColor(this.classes.JavaColor.black);
+    graphics.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
 
-  var bounds = component.getBounds();
-
-  graphics.setColor(JavaColor.black);
-  graphics.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
-
-  if (checked) {
-    graphics.setColor(JavaColor.green);
-    graphics.drawLine(bounds.x + 2, bounds.y + 2, bounds.x + bounds.width - 2, bounds.y + bounds.height - 2);
-    graphics.drawLine(bounds.x + 2, bounds.y + bounds.height - 2, bounds.x + bounds.width - 2, bounds.y + 2);
+    if (checked) {
+      graphics.setColor(this.classes.JavaColor.green);
+      graphics.drawLine(bounds.x + 2, bounds.y + 2, bounds.x + bounds.width - 2, bounds.y + bounds.height - 2);
+      graphics.drawLine(bounds.x + 2, bounds.y + bounds.height - 2, bounds.x + bounds.width - 2, bounds.y + 2);
+    }
   }
-}
+});
