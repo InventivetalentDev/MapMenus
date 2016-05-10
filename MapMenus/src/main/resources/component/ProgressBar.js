38898219a0fda993ea2ed6627932c7dfb7f6bdaf
inventivetalent <inventivetalent@inventivegames.de>
2016-05-11 00:17:10 +0200

    make colors customizable

--- MapMenus/src/main/resources/component/ProgressBar.js
+++ MapMenus/src/main/resources/component/ProgressBar.js
@@ -1,8 +1,16 @@
 // ProgressBar
 // author: inventivetalent
+//
+// Use colors.border, colors.bar, colors.background to change colors
+// Use setProgress(progress)/getProgress() to modify the progress value
 ({
     classes: {
         JavaColor: Java.type("java.awt.Color")
+    },
+    colors: {
+        border: this.classes.JavaColor.DARK_GRAY,
+        bar: this.classes.JavaColor.BLUE,
+        background: this.classes.JavaColor.WHITE
     },
     progress: 0,
     setProgress: function(progress) {
@@ -14,10 +22,13 @@
     render: function(graphics, player) {
         var bounds = this.component.getBounds();
 
-        graphics.setColor(this.classes.JavaColor.BLUE);
+        graphics.setColor(this.colors.background);
+        graphics.fillRect(bounds.x, bounds.y, bounds.width, bounds.height);
+
+        graphics.setColor(this.colors.bar);
         graphics.fillRect(bounds.x, bounds.y, (this.progress * bounds.width / 100), bounds.height);
 
-        graphics.setColor(this.classes.JavaColor.DARK_GRAY);
+        graphics.setColor(this.colors.border);
         graphics.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
     }
 });
