f179bce9d4cbcd45145e43f346ae77639c553505
inventivetalent <inventivetalent@inventivegames.de>
2016-05-10 02:51:29 +0200

    update & fix BasicLabel | add borderVisible

--- MapMenus/src/main/resources/component/BasicLabel.js
+++ MapMenus/src/main/resources/component/BasicLabel.js
@@ -1,12 +1,31 @@
-var text;
-
-function init(t) {
-    text = t;
-}
-
-// Called when this element is rendered
-function render(graphics,player) {
-   var bounds = component.getBounds();
-    graphics.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
-    renderer.drawStringCentered(graphics, text, bounds);
-}
+({
+  classes: {
+    JavaColor: Java.type("java.awt.Color")
+  },
+  text: "",
+  borderVisible: false,
+  setText: function(text) {
+    this.text = text;
+  },
+  getText: function() {
+    return this.text;
+  },
+  setBorderVisible: function(borderVisible) {
+    this.borderVisible = borderVisible;
+  },
+  isBorderVisible: function() {
+    return this.borderVisible;
+  },
+  init: function(text, borderVisible) {
+    this.text = text;
+    this.borderVisible = borderVisible;
+  },
+  render: function(graphics, player) {
+    var bounds = this.component.getBounds();
+    graphics.setColor(this.classes.JavaColor.black);
+    if (this.borderVisible) {
+      graphics.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
+    }
+    this.menu.renderer.drawStringCentered(graphics, bounds, this.text);
+  }
+});
